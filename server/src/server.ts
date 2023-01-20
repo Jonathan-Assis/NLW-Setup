c01e39d1ce8055f958998e3906bdae523e4c8b66
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-20 00:43:54 -0300

    Installing cors and create example of query
    Using 'app.register(cors)' all the front application can use the database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,11 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
 
 /**
  * Método HTTP: Get, Post, Put, Patch, Delete
@@ -8,8 +13,16 @@ const app = Fastify()
  * Abaixo foi utilizado como exemplo a rota .get no http://localhost:3333/
  */
 
-app.get('/', () => {
-    return 'Hello World!'
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
 })
 
 app.listen({
