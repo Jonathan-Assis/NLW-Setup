3e3e7debd883958136fed1679c13134db925126f
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-21 02:31:28 -0300

    Organizing accesses

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-/**
- * Método HTTP: Get, Post, Put, Patch, Delete
- * 
- * Abaixo foi utilizado como exemplo a rota .get no http://localhost:3333/
- */
-
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333
