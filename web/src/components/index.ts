c19d1b864d05d763cc146790da23ffc4ffc75042
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-21 17:09:50 -0300

    Create a shortcut of components

---
+++ web/src/components/index.ts
@@ -0,0 +1,9 @@
+import { Header } from "./Header";
+import { SummaryTable } from "./SummaryTable";
+import { HabitDay } from "./HabitDay";
+
+export {
+    Header,
+    SummaryTable,
+    HabitDay,
+}
