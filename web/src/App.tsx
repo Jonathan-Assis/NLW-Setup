deca24910928c4c3b7e40b36215290432e2f3b99
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-21 17:10:19 -0300

    Add components to show on the screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,16 @@
 import './styles/global.css'
-import { Habit } from "./components/Habit"
+import { Header, SummaryTable } from './components'
 
-function App() {
+export function App() {
 
   return (
-    <div>
-      <Habit completed={5} />
-      <Habit completed={8} />
-      <Habit completed={8} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+        <Header />
+        <SummaryTable />
+
+      </div>
     </div>
   )
 }
-
-export default App
