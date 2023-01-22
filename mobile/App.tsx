01a20e4243211630b5cb50c69bef0284314bcacc
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-22 20:35:44 -0300

    Add routes in App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
 
 import { Loading } from './src/components';
-import { Home } from './src/screens';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +16,7 @@ export default function App() {
     !fontsLoaded ? <Loading />
     :
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
