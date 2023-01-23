c0eb322f57a1cde9361a34e19e13f0c606552614
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-22 21:20:26 -0300

    Add components in shortcut

--- mobile/src/components/index.ts
+++ mobile/src/components/index.ts
@@ -1,8 +1,12 @@
+import { BackButton } from './BackButton'
+import { Checkbox } from './Checkbox';
 import { HabitDay } from "./HabitDay";
 import { Header } from "./Header";
 import { Loading } from "./Loading";
 
 export {
+    BackButton,
+    Checkbox,
     HabitDay,
     Header,
     Loading
