4500d6d9c1013942ed9202f490db79fdbf6ff184
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-23 00:55:13 -0300

    Create a shortcut of utils

---
+++ mobile/src/utils/index.ts
@@ -0,0 +1,7 @@
+import { generateRangeDatesFromYearStart } from './generate-range-between-dates';
+import { generateProgressPercentage } from './generate-progress-percentage';
+
+export {
+    generateRangeDatesFromYearStart,
+    generateProgressPercentage
+}
