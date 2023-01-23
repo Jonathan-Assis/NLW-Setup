a5b001a259427f3b871cbea801aeb70fa5a599e5
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-23 02:23:20 -0300

    Add component

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,4 +1,5 @@
 import { HabitDay } from "./HabitDay";
+import { HabitsList } from "./HabitsList";
 import { Header } from "./Header";
 import { NewHabitForm } from "./NewHabitForm";
 import { ProgressBar } from "./ProgressBar";
@@ -6,6 +7,7 @@ import { SummaryTable } from "./SummaryTable";
 
 export {
     HabitDay,
+    HabitsList,
     Header,
     NewHabitForm,
     ProgressBar,
