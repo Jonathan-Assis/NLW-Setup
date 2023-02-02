a7fa61abf15c4a81422a0b8c675ca0b340596c44
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-02-01 22:38:11 -0300

    :star: Create simple notification when open the app :jigsaw:

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,19 @@
 import './src/lib/dayjs';
 import { StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import * as Notifications from 'expo-notifications';
 
 import { Loading } from './src/components';
 import { Routes } from './src/routes';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+})
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,7 +21,30 @@ export default function App() {
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_800ExtraBold
-  })
+  });
+
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Mantenha o foco! 🎯',
+        body: 'Você já completou algum hábito hoje?'
+      },
+      trigger: {seconds: 2}
+    });
+  }
+
+  async function getScheduleNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedules);
+  }
+
+  useEffect(() => {
+    scheduleNotification()
+    getScheduleNotification()
+  },[])
 
   return (
     !fontsLoaded ? <Loading />
